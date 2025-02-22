d0d3dc845950b2dab429812f2ce1c5e094206c8b
i2PieO <jawadsyed406@gmail.com>
2025-02-22 03:58:30 +0300

    Setting Up Themes

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:amazon/constants/global_variables.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,16 +13,33 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Amazon',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: GlobalVariables.backgroudColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: const AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(
+            color: Colors.black,
+          ),
+        ),
       ),
       home: Scaffold(
         appBar: AppBar(
           title: const Text('Hello.'),
         ),
-        body: const Center(
-          child: Text(
-            'Flutter Demo Home Page',
-          ),
+        body: Column(
+          children: [
+            const Center(
+              child: Text(
+                'Flutter Demo Home Page',
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text('Click'),
+            ),
+          ],
         ),
       ),
     );
