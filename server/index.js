8b0309ab4902297859aa3b57e41ef8ea872a5d97
i2PieO <jawadsyed406@gmail.com>
2025-03-04 15:01:36 +0300

    Create your First A.P.I.

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const PORT = 3000;
 const app = express();
 
 // CREATING AN API
-app.listen(PORT, "0.0.0.0", () => {
+// http://<youripaddress>/hello-world
+app.get("/hello-world", (req, res) => {
+  res.json({hi:"hello world"});
+});
+// GET, PUT, POST, DELETE, UPDATE -> CRUD
+
+app.listen(PORT, () => {
     console.log(`connected at port ${PORT}`);
 });
