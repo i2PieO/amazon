68ef1b8b54cb27ceaa41e031c6642943e2701dce
i2PieO <jawadsyed406@gmail.com>
2025-03-14 07:15:45 +0300

    Connecting to MongoDB

--- server/index.js
+++ server/index.js
@@ -7,10 +7,11 @@ const authRouter = require("./routes/auth");
 // INIT
 const PORT = 3000;
 const app = express();
-const DB = "mongodb+srv://jawad:jawad@cluster0.qlhih.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const DB =
+  "mongodb://jawad:jawad123@cluster0-shard-00-00.qlhih.mongodb.net:27017,cluster0-shard-00-01.qlhih.mongodb.net:27017,cluster0-shard-00-02.qlhih.mongodb.net:27017/?ssl=true&replicaSet=atlas-2am48c-shard-0&authSource=admin&retryWrites=true&w=majority&appName=Cluster0";
 
 // middleware
-app.use(express.json());  
+app.use(express.json());
 app.use(authRouter);
 
 // Connections
