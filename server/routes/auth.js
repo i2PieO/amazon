b380df9e5ec89b39b8d251f5356fa7e35f3e17f1
i2PieO <jawadsyed406@gmail.com>
2025-04-28 06:58:11 +0300

    Sign up Route cntd

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const User = require("../models/user");
-
+const bcryptjs = require('bcryptjs');
 const authRouter = express.Router();
 
+// SIGN UP 
 authRouter.post("/api/signup", async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -13,10 +14,12 @@ authRouter.post("/api/signup", async (req, res) => {
         .status(400)
         .json({ msg: "User with same email already exists!" });
     }
-
+    
+    const hashedPassword = await bcryptjs.hash(password, 8);
+    
     let user = new User({
       email,
-      password,
+      password: hashedPassword,
       name,
     });
     user = await user.save();
